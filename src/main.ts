e2a2c38641ab9679881730c92d9c7a1a66a881f3
cxq1239035450 <1239035450@qq.com>
2024-05-09 04:51:22 +0800

    fix:更新代码

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import './icon/iconfont.css'
 import 'virtual:uno.css'
 import router from './router'
 import './permission'
-import ElementPlus from 'element-plus'
+// import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 window.document.documentElement.setAttribute('data-theme', 'light')
 
-createApp(App).use(ElementPlus).use(router).use(createPinia()).mount('#app')
+createApp(App).use(router).use(createPinia()).mount('#app')
